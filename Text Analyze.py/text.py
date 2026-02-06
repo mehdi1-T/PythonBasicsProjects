581902631581440ba50fbdab8301465115b6609c
mehdii-tal**** <mehditalalha29@gmail.com>
2026-02-06 14:54:24 +0100

    README.md update

---
+++ Text Analyze.py/text.py
@@ -0,0 +1,30 @@
+# this project takes from the user txt and analyse it
+"""
+Doc1️⃣ Text Analyzer (🔥 must-do)
+
+Build a program that takes a text and outputs:
+
+Number of characters
+
+Number of words
+
+Number of sentences
+
+Most common word
+
+Longest & shortest word
+
+Percentage of vowels / consonants
+
+Concepts used
+
+.split(), .count(), loops
+
+dictionaries
+
+string cleaning
+
+👉 This feels simple but gets tricky fast (punctuation, spaces, casing).string for Text Analyze.py.text
+"""
+
+text = input("Text: ")
